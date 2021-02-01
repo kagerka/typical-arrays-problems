1181349a3019b0301faf592211cb995dbec1c512
kagera <52308117+kagerka@users.noreply.github.com>
2021-02-01 10:51:48 +0300

    feat: add functions for min, max and avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    } else {
+        let arr = array.sort((a, b) => a - b);
+        return arr[0];
+    }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    } else {
+        let arr = array.sort((a, b) => b - a);
+        return arr[0];
+    }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    } else {
+        let arr = array.reduce((a, b) => a + b);
+        return (arr / array.length);
+    }
 }
